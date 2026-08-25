7da471238aee386bbe92e2e8e5b3395788f53f9e
MARTIN DAWIDZON <50031557@ort>
2026-08-25 08:06:35 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sala_de_escape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
